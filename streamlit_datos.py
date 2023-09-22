f8e25b01e4107bac1338b2497a9d580715f03f44
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-09-22 10:04:20 -0300

    Update streamlit_datos.py

--- streamlit_datos.py
+++ streamlit_datos.py
@@ -93,7 +93,7 @@ if aux_contra == True :
 
     st.write("Cálculos por programa")
     calculos_por_programa = lista_tablas[1].to_html(index=False, escape=False)
-    calculos_por_programa = calculos_por_programa.replace('<table border="1" class="dataframe">', '<table style="width: 100%;" border="1" class="dataframe">')
+    calculos_por_programa = calculos_por_programa.replace('<table border="1" class="dataframe">', '<table style="width: 100%;" border="1" class="dataframe">''<th>', '<th style="text-align: center;">')
     st.write(calculos_por_programa, unsafe_allow_html=True)
     st.write("---")
 
