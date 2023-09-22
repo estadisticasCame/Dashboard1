97c42d2489f050ccc1fc22f61923edd49cc2cbb5
Estadísticas e Informes <143653599+estadisticasCame@users.noreply.github.com>
2023-09-22 10:09:08 -0300

    Update streamlit_datos.py

--- streamlit_datos.py
+++ streamlit_datos.py
@@ -93,9 +93,10 @@ if aux_contra == True :
 
     st.write("Cálculos por programa")
     calculos_por_programa = lista_tablas[1].to_html(index=False, escape=False)
-    # Aplicar estilos CSS para el ancho y centrar contenido
     calculos_por_programa = calculos_por_programa.replace('<table border="1" class="dataframe">',
                                     '<table style="width: 100%; text-align: center;" border="1" class="dataframe">')
+    calculos_por_programa = calculos_por_programa.replace('<th>', '<th style="text-align: center;">')
+    
     # Mostrar la tabla en Streamlit
     st.write(calculos_por_programa, unsafe_allow_html=True)
     st.write("---")
